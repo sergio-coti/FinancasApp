b7cf99ce6ac9068499c7258ed4d96d8c6089cfcd
sergio.coti <cotiaulajava@outlook.com>
2024-09-23 11:42:16 -0300

    Alteração da string de conexão

--- FinancasApp.Infra.Data/Settings/SqlServerSettings.cs
+++ FinancasApp.Infra.Data/Settings/SqlServerSettings.cs
@@ -12,6 +12,7 @@ namespace FinancasApp.Infra.Data.Settings
     /// </summary>
     public class SqlServerSettings
     {
-        public static string ConnectionString => "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDFinancasApp;Integrated Security=True;";
+        public static string ConnectionString => "Data Source=cotisqlserver.database.windows.net;Initial Catalog=bdcoti;User ID=usercoti;Password=Coti2024!;Encrypt=False";
+        //public static string ConnectionString => "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDFinancasApp;Integrated Security=True;";
     }
 }
